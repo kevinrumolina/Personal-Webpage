73c29983f9e98194be9b946bb2b5dbf22ed036bb
kevinrumolina <kevinrumolina26@gmail.com>
2025-02-08 20:07:05 -0500

    kerumo website deploy

---
+++ main.js
@@ -0,0 +1,77 @@
+const setPink = li => {
+    li.className = "list-item border-fucsia";
+}
+
+const resetGrey = array => {
+    array.forEach(li => {
+        li.className = "list-item border-gray";
+    })
+}
+
+const createElement = (element, text) => {
+    const tag = document.createElement(element);
+
+    tag.innerText = text;
+
+    return tag;
+}
+
+const createTitle = (title, company) => {
+    const span = createElement('span', ` @ ${company}`);
+    const h3 = createElement ('h3', title);
+
+    h3.appendChild(span);
+
+    return h3;
+}
+
+const createDate = (date) => createElement('p', date);
+
+const createTask = (task) => createElement('li', task);
+
+const createDescription = (array) => {
+    const ul = document.createElement('ul');
+
+    array.forEach(item => {
+        ul.appendChild(createTask(item));
+    })
+
+    return ul;
+}
+
+const companiesData = async() => { 
+    const response = await fetch("./data/companies.json");
+    const jsonResponse = await response.json();
+    const { companies } = jsonResponse;
+
+    const parentDescription = document.querySelector('.company-description');
+
+    setPink(document.querySelector('.list-item'));
+
+    document.querySelectorAll('.list-item').forEach(li => {
+        li.addEventListener("click", () => {
+            resetGrey(document.querySelectorAll('.list-item'));
+            setPink(li);
+            parentDescription.innerHTML = "";
+
+            const [ company ] = companies.filter(company => company.name == li.innerText)
+            parentDescription.appendChild(createTitle(company.title, company.name));
+            parentDescription.appendChild(createDate(company.dates));
+            parentDescription.appendChild(createDescription(company.tasks));
+        })
+    })
+}
+
+const addCloseMenuEvent = () => {
+    const menuItems = document.querySelectorAll('.menu li');
+    const burgerInput = document.querySelector('#burger');
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            burgerInput.checked = false;
+        })
+    })
+}
+
+companiesData();
+addCloseMenuEvent();
